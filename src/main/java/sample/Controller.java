613a47680b8e83d76cbafc1063c8cc2ba2cf6060
Sergey Yakovlev <web-wind@ya.ru>
2020-02-21 11:37:13 +0300

    commit

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,4 +1,25 @@
 package sample;
 
+import javafx.scene.control.*;
+import javafx.scene.web.HTMLEditor;
+import javafx.stage.FileChooser;
+
 public class Controller {
+    private final FileChooser chooser = new FileChooser();
+    public TextField from;
+    public TextField subject;
+    public TextField smtpServer;
+    public TextField userName;
+    public PasswordField smtpPassword;
+    public TableColumn<MailStatus, String> mailColumn;
+    public TableColumn<MailStatus, Boolean> sentColumn;
+    public TableView<MailStatus> mailView;
+    public HTMLEditor htmlEditor;
+    public ComboBox<String> mimeType;
+
+    public void openFile() {
+    }
+
+    public void sendMassive(){}
+
 }
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -6,13 +6,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Mail Sender");
+
+        primaryStage.setScene(new Scene(root, 860, 660));
         primaryStage.show();
     }
 
